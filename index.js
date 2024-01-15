fbdb5fd38ac719915f562d57e444dbf40ab63224
anayetabir <anayetabir007@gmail.com>
2024-01-16 03:30:17 +0600

    updated Successfully

--- index.js
+++ index.js
@@ -70,6 +70,25 @@ async function run() {
 
         })
 
+        app.put('/coffee/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const options = { upsert: true };
+            const updatedCoffee=req.body;
+
+            const coffee={
+                $set:{
+                    name:updatedCoffee.name, quantity:updatedCoffee.quality, supplier:updatedCoffee.supplier, taste:updatedCoffee.taste, category:updatedCoffee.category, details:updatedCoffee.details, 
+                    photo:updatedCoffee.photo
+                }
+            }
+            const result=await coffeeCollection.updateOne(filter,coffee,options);
+            res.send(result);
+
+        })
+
+
+
 
 
 
